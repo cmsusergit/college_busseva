8aa1f7ce276e02c37768035780a6ee0a236b7e9f
gpuuser <svitrndvasad@gamil.com>
2023-07-28 11:10:50 +0530

    test done

--- src/routes/response/+page.svelte
+++ src/routes/response/+page.svelte
@@ -11,7 +11,7 @@ onMount(()=>{
     if(data.response?.order_status.includes("Success")){
         responseList.push({name:"Record ID",value:data.response?.order_id})
 
-        responseList.push({name:"Name",value:data.response?.billing_name})    
+        responseList.push({name:"Name",value:data.response?.billing_name})     
         responseList.push({name:"Email",value:data.response?.billing_email})
         responseList.push({name:"Tracking ID",value:data.response?.tracking_id})        
         responseList.push({name:"Payment Mode",value:data.response?.payment_mode})
