24343d2862ec0a429d5803816ecd302170112350
gpuuser <svitrndvasad@gamil.com>
2023-07-28 11:13:16 +0530

    razorpay removed

--- src/routes/api/payment/+server.js
+++ src/routes/api/payment/+server.js
@@ -2,20 +2,6 @@ import { json, text } from '@sveltejs/kit'
 import { env } from '$env/dynamic/public'
 
 import {encrypt} from '$lib/util/ccavutil'
-import Razorpay from 'razorpay'
-export const GET=async(event)=>{
-    const orderid=event.url.searchParams.get('orderid')
-    const rzrpay1= new Razorpay({ key_id: 'rzp_test_JYdzDk5tUT2JJR', key_secret: 'xa9HFuV4XeapBYVqNE9AX3AL' })    
-    try{
-        const order1=await rzrpay1.orders.fetch(orderid)
-        console.log(order1)
-        return json(order1)
-
-    }catch(error1){
-        console.log(error1)        
-        return json(error1)
-    }
-}
 export const POST=async(event) =>{
     const dt=await event.request.json()
     try{
