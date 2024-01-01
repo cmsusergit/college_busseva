cf2fb3380f1bfb5187a87d5224f09c49d7454e97
gpuuser <svitrndvasad@gamil.com>
2024-01-01 16:05:37 +0530

    tested done

--- src/routes/response/+page.svelte
+++ src/routes/response/+page.svelte
@@ -67,7 +67,7 @@ const printReceipt=()=>{
                 <Button on:click={printReceipt} class="w-48 rounded">Receipt</Button>
             </div>
         {/if}
-        <a href="/" class="w-48 bg-blue-500 rounded text-center">Go to Form Again</a>
+        <a href="/" class="w-48 p-2 bg-blue-500 text-white rounded text-center">Go to Form Again</a>
     </div>
     {/if}
 </div>
