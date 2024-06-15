650b2dfbaa4df447e9d0a33f9dde4be3cfa9c1e3
gpuuser <svitrndvasad@gamil.com>
2024-06-15 15:04:53 +0530

    text done

--- src/routes/qrscanner/+page.svelte
+++ src/routes/qrscanner/+page.svelte
@@ -72,13 +72,13 @@
     }
 </script>
 <div>
-
     {text}
     {#if !currRecord}
-    <div id="reader" width="800"/>
-    <input on:change={onchange} type="file" name="" id="">
+        <div id="reader" width="800"/>
+        <input on:change={onchange} type="file" name="" id="">
     {:else}
         <div>
+            ----{JSON.stringify(currRecord)}----
             <Profile profile={currRecord}></Profile>  
         </div>
     {/if}
